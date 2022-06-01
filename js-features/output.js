f06be4d5bec7aba2f1207a33b3b505369ffe3768
myplace-e-hub <ravicodeproject@gmail.com>
2022-06-01 11:11:12 +0530

    Javascript Output

---
+++ js-features/output.js
@@ -0,0 +1,13 @@
+document.getElementById("textInnerHtml").innerHTML="Hello My Place E";
+document.getElementById("htmlInnerHtml").innerHTML="<div style='color:red;'>Hello My Place E</div>";
+
+document.getElementById("textInnerTEXT").innerText="Hello My Place E";
+document.getElementById("htmlInnerTEXT").innerText="<div style='color:red;'>Hello My Place E</div>";
+
+document.write('Hello Info2doc from script')
+document.write('<div style="color:blue">Hello Info2doc from script</div>')
+
+window.alert('hello myplace e');
+alert('hello myplace e');
+
+console.log('hello myplace e');
